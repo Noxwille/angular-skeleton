df3c2b697332fcd23f9c7c95ce9749cebc56d0fa
Bogdan Pohasii <bogdanpogasiy@gmail.com>
2015-10-15 12:39:33 +0300

    Modify gulp configuration (Add single conf file) Add "fonts" task Add "images" task, add minify images Add autoprefixer and minify to "styles" task Improve "clean" task

---
+++ gulp_tasks/scripts.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var gulp        = require('gulp'),
+    plumber     = require('gulp-plumber'),
+    sourcemaps  = require('gulp-sourcemaps'),
+    requirejs   = require('requirejs'),
+    browserSync = require('browser-sync'),
+    $           = require('../config/gulpConfig');
+
+gulp.task($.TASK.scripts.name, buildScripts);
+
+gulp.task($.TASK.scriptsMinify.name, prodScripts);
+
+//todo: throw new Error('toString failed') with source maps
+function buildScripts () {
+    return gulp.src($.TASK.scripts.src, $.CONFIG.scripts.base)
+        .pipe(plumber())
+        //.pipe(sourcemaps.init())
+        //.pipe(sourcemaps.write())
+        .pipe(gulp.dest($.TASK.scripts.dest))
+        .pipe(browserSync.reload({stream:true}));
+}
+
+function prodScripts () {
+    return requirejs.optimize($.CONFIG.scripts.requirejs);
+}
+
